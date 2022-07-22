49e2b4da00a3c6705a790f4fea1a94dd4e407c27
“Salhiaa” <marie-amelie.delerue@outlook.fr>
2022-07-22 18:19:41 +0200

    Crow and MrFiery Animation
    Animation for crow flying, mr fiery walking (without script) and deleting duplicated sprites in player walk animation

--- BeforeTheEnd/Assets/Scripts/Corbeau/CorbeauMovement.cs
+++ BeforeTheEnd/Assets/Scripts/Corbeau/CorbeauMovement.cs
@@ -5,30 +5,35 @@ using UnityEngine.InputSystem;
 
 public class CorbeauMovement : MonoBehaviour
 {
-    [SerializeField] Transform ravenRestPos;
+    [SerializeField] Transform RestPos;
+    [SerializeField] Transform centerCelling;
+
+    [SerializeField] Animator animCrow;
 
     public bool wantsToRest = false;
     public bool isUsingVision, isAtCelling;
 
-    [SerializeField] Transform centerCelling;
+    
 
     private void FixedUpdate()
     {
         // Revient sur la Tete
-        if (wantsToRest && (transform.position != ravenRestPos.position))
+        if (wantsToRest && (transform.position != RestPos.position))
         {
             isUsingVision = false;
-            transform.position = Vector3.MoveTowards(transform.position, ravenRestPos.position, .15f);
-        } else if (wantsToRest && transform.position == ravenRestPos.position) // Quand est sur la tete, arrete de vouloir etre sur la tete
+            transform.position = Vector3.MoveTowards(transform.position, RestPos.position, .15f);
+        } else if (wantsToRest && transform.position == RestPos.position) // Quand est sur la tete, arrete de vouloir etre sur la tete
         {
             isAtCelling = false;
             wantsToRest = false;
+            animCrow.SetBool("isFlying", false);
         }
 
         // Va au plafond
         if (!isAtCelling && isUsingVision && transform.position != centerCelling.position)
         {
             transform.position = Vector3.MoveTowards(transform.position, centerCelling.position, .15f);
+            animCrow.SetBool("isFlying", true);
         } else if (isUsingVision && transform.position == centerCelling.position) // Quand est au plafond, arrete de vouloir allez au plafond
         {
             isAtCelling = true;
@@ -38,7 +43,7 @@ public class CorbeauMovement : MonoBehaviour
 
     public void Rest()
     {
-        if(transform.position != ravenRestPos.position)
+        if(transform.position != RestPos.position)
         {
             transform.SetParent(GameObject.FindGameObjectWithTag("Player").transform);
             wantsToRest = true;
